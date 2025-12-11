442a734824dd07528f69923dd0d4d4a4a6f83b7c
Krishna Vishwakarma <krishhnna02@gmail.com>
2025-12-11 08:48:25 +0530

    restrict certain routes

---
+++ Backend/middleware/roleMiddleware.js
@@ -0,0 +1,22 @@
+const authorize = (...roles) => {
+    return (req, res, next) => {
+        // req.user is set by the 'protect' middleware
+        if (!req.user) {
+            return res.status(401).json({
+                success: false,
+                message: 'Not authorized, user not found'
+            });
+        }
+
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({
+                success: false,
+                message: `User role '${req.user.role}' is not authorized to access this route`
+            });
+        }
+
+        next();
+    };
+};
+
+module.exports = { authorize };
